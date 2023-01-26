fddc204c2a6a296c1cabc81a7dbd97a8a94fe581
diegodpcarvalho <diegodpcarvalho@gmail.com>
2023-01-26 20:57:38 -0300

    Aplicando controle de perfil de usuário

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -1,24 +1,28 @@
 <?php 
-  require_once "validador_acesso.php";
-
-  $chamados = array();
-
-  //abrir o arquivo.txt
-  $arquivo = fopen('arquivo.txt', 'r' );
-
-  //enquamtp houver registro (linhas) a serem recuperadas
-  while(!feof($arquivo)) { //testa pelo fim de um arquivo
-    $registro = fgets($arquivo);
-    $chamados[] = $registro;
-
-  }
-
-  //fechar o arquivo aberto
-  fclose($arquivo);
+require_once "validador_acesso.php";
 
 ?>
 
+<?php
 
+  //array de chamados
+
+  $chamados = array();
+  //http://php.net/manual/pt_BR/function.fopen.php
+  //abrir arquivo.txt
+  $arquivo = fopen('arquivo.txt','r');
+
+  //enquato houverem registros (linhas) a serem recuperados
+  while(!feof($arquivo)){ //testa pelo fim do arquivo
+    //linhas
+    $registro = fgets($arquivo);//recupera a linha
+    $chamados[] = $registro;
+  }
+
+  //fechando o arquivo.txt
+  fclose($arquivo);
+
+?>
 <html>
   <head>
     <meta charset="utf-8" />
@@ -41,11 +45,14 @@
       <a class="navbar-brand" href="#">
         <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
         App Help Desk
-        </a>
-      <ul class ="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="logoff.php"> SAIR</a>
       </a>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a href="logoff.php" class="nav-link">
+          SAIR
+          </a>
+        </li>
+      </ul>
     </nav>
 
     <div class="container">    
@@ -59,31 +66,36 @@
             
             <div class="card-body">
               
-              <? foreach($chamados as $chamado) {?>
-                
-                <?php
+              <?php foreach($chamados as $chamado){ ?>
+
+              <?php
                 $chamado_dados = explode('#', $chamado);
-                
-                if(count($chamado_dados) < 3) {
+
+                if($_SESSION['perfil_id'] == 2){
+                  //apenas exibir se foi criado pelo mesmo usuario
+                  if($chamado_dados[0] != $_SESSION['id']){
+                    continue;
+                  }
+                }
+
+                if(count($chamado_dados) < 3){
                   continue;
                 }
-              
-                ?>
-              
-                <div class="card mb-3 bg-light">
+              ?>
+              <div class="card mb-3 bg-light">
                 <div class="card-body">
-                  <h5 class="card-title"><?=$chamado_dados[0]?></h5>
-                  <h6 class="card-subtitle mb-2 text-muted"><?=$chamado_dados[1]?></h6>
-                  <p class="card-text"><?=$chamado_dados[2]?></p>
+                  <h5 class="card-title"><?= $chamado_dados[1]?></h5>
+                  <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[2]?></h6>
+                  <p class="card-text"><?= $chamado_dados[3]?></p>
+
                 </div>
               </div>
-             <? } ?> 
-              
-              
+
+              <?php } ?>
 
               <div class="row mt-5">
                 <div class="col-6">
-                  <a class="btn btn-lg btn-warning btn-block" href="home.php">Voltar</a>
+                  <a href="home.php" class="btn btn-lg btn-warning btn-block">Voltar</a>
                 </div>
               </div>
             </div>
